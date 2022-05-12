0caff85283f46aa4fcc2b628c4f94945b5bce020
Roykesydon <roy23381547@gmail.com>
2022-05-12 17:21:22 +0800

    style: formatting code

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ from utils.SecurityGuard import SecurityGuard
 config = get_config()
 security_guard = SecurityGuard()
 
-iv, encypt_message = security_guard.encrypt_message("xxx service is dead!", config["key"])
+iv, encypt_message = security_guard.encrypt_message(
+    "xxx service is dead!", config["key"]
+)
 print(iv, encypt_message)
-
-
